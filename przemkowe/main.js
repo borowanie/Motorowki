8848daf22938d3345b4d8554177498b21b2e61e9
borowanie <przemek>
2014-03-16 13:17:46 +0100

    generowanie satkow, bez zab
    Statki generuje, ale nie ma zabezpieczeń względem innych statków ani
    końców planszy

--- przemkowe/main.js
+++ przemkowe/main.js
@@ -1,5 +1,8 @@
 function main () {
+	temparr = []; //zmienna tylko do usuwania śladów
+	lasttemp = 0;//podobnie jak wyzej
 	shipLength=0;
+	shiptmp = 0;
 	createArray();
 	createTable();
 	createShipButtons();
@@ -58,9 +61,12 @@ function createTable(){
 	             	}else{
 	             		var cellContent = document.createElement("input");
 	             		cellContent.setAttribute("type","button");
-	             		cellContent.setAttribute("id",j+'.'+i);
+	             		a = j-1;
+	             		b = i-1;
+	             		cellContent.setAttribute("id",a+'.'+b);
 	             		cellContent.setAttribute("style","width: 24px;");
 	             		cellContent.setAttribute("onclick","shipClick(this.id)");
+	             		cellContent.disabled = true;
 	             	}
 	                cell.appendChild(cellContent);
 	                row.appendChild(cell);
@@ -75,32 +81,105 @@ function createTable(){
 }
 
 function setShip(tmp){
-	console.log(tmp);
+	//console.log(tmp);
 	shipLength = tmp;
+	for (var j = 0; j <= 9; j++) {
+		for (var i = 0; i < 9; i++) {
+			var bt = document.getElementById(j+"."+i);
+			bt.disabled = false;
+		}
+	}
 }
 
 function shipClick(aa){
-	//console.log("no klikło "+aa);
+	shiptmp = aa;
+	for (var j = 0; j <= 9; j++) {
+		for (var i = 0; i < 9; i++) {
+			var bt = document.getElementById(j+"."+i);
+			bt.disabled = true;
+		}
+	}
+
 	if (shipLength>0) {
 		shipLength--;
+		var x = aa.charAt(0);
+		var y = aa.charAt(2);
+
 		var tmp  = document.getElementById(aa);
 		tmp.setAttribute("style","background-Color:#556;width: 24px;");
-		var bb = aa.charAt(0)-1+"."+aa.charAt(2);
-		var means = document.getElementById(bb);
-		means.setAttribute("style","background-Color:#357EC7;width: 24px;");
+		if (shipLength>0) {
+			var bb = x-1+"."+y;
+			var means = document.getElementById(bb);
+			temparr.push(bb);
+			means.disabled = false;
+			means.setAttribute("style","background-Color:#357EC7;width: 24px;");
+			means.setAttribute("onclick","drawShip(this.id)");
 
-		bb = aa.charAt(0)-1+2+"."+aa.charAt(2); 
-		means = document.getElementById(bb);
+			var b1 = x-1+2+"."+y; 
+			means = document.getElementById(b1);
+			temparr.push(b1);
+			means.disabled = false;
+			means.setAttribute("style","background-Color:#357EC7;width: 24px;");
+			means.setAttribute("onclick","drawShip(this.id)");
 
-		means.setAttribute("style","background-Color:#357EC7;width: 24px;");
+			y--
+			var b2 = x+"."+y;
+			means = document.getElementById(b2);
+			temparr.push(b2);
+			means.disabled = false;
+			//console.log(aa +"  " + bb +"  "+ b1+"  "+ b2);
+			means.setAttribute("style","background-Color:#357EC7;width: 24px;");
+			means.setAttribute("onclick","drawShip(this.id)");
+			y+=2;
+			var b3 = x+"."+y;
+			means = document.getElementById(b3);
+			temparr.push(b3);
+			means.disabled = false;
+			means.setAttribute("style","background-Color:#357EC7;width: 24px;");
+			means.setAttribute("onclick","drawShip(this.id)");
+		};
+	};
+}
 
-		bb = aa.charAt(0)+"."+aa.charAt(2)-1;
-		means = document.getElementById(bb);
-		console.log(bb);
-		means.setAttribute("style","background-Color:#357EC7;width: 24px;");
+function drawShip(temp){
 
-		bb = aa.charAt(0)+"."+aa.charAt(2)-1+2;
-		means = document.getElementById(bb);
-		means.setAttribute("style","background-Color:#357EC7;width: 24px;");
+	for (var i = temparr.length - 1; i >= 0; i--) {
+		if (temparr[i]!=lasttemp) {
+			var mleko = document.getElementById(temparr[i]);
+			mleko.setAttribute("style","width: 24px;");
+			mleko.disabled = true;
+		};
+	};
+	temparr = [];
+	lasttemp = temp;
+	var x = shiptmp.charAt(0);
+	var y = shiptmp.charAt(2);
+
+	var x1 = temp.charAt(0);
+	var y1 = temp.charAt(2);
+
+	if ((x-x1)!=0) {
+		wektorx = x-x1;
+		wektory = 0;
+	}else if ((y-y1)!=0) {
+		wektory = y-y1;
+		wektorx = 0;
+	};
+
+
+	for (var i = 1; i <= shipLength; i++) {
+		if (wektorx==0) {
+			y-= wektory;
+
+			console.log(x+"."+y);
+			part = document.getElementById(x+"."+y);
+			part.setAttribute("style","background-Color:#556;width: 24px;");
+		}else if (wektory==0) {
+			x-= wektorx;
+			console.log(x+"."+y);
+			part = document.getElementById(x+"."+y);
+			part.setAttribute("style","background-Color:#556;width: 24px;");
+
+		};
 	};
 }
