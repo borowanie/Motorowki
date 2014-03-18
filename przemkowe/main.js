3e735b7adb2e0345c63bebc03c877f9e120664ef
borowanie <przemek>
2014-03-18 01:46:24 +0100

    W chuja zabezpieczen
    ale nie wszystkie, jest jedna rzecz... ale kurwa, nie mam pomysłu

--- przemkowe/main.js
+++ przemkowe/main.js
@@ -16,12 +16,17 @@ function main () {
 
 function createArray(){
 	sendarr = new Array(10);
-	whitestain = new Array(10);
 	for (var i = 0; i < sendarr.length; i++) {
 	    sendarr[i] = new Array(10);
-	    whitestain[i] = new Array(10);
 	    for(var j = 0; j < sendarr[i].length; j++ ) {
 	       sendarr[i][j] = "0";
+	    }
+	}
+
+	whitestain = new Array(12);
+	for (var i = 0; i < whitestain.length; i++) {
+	    whitestain[i] = new Array(12);
+	    for(var j = 0; j < whitestain[i].length; j++ ) {
 	       whitestain[i][j] = "1";
 	    }
 	}
@@ -64,11 +69,40 @@ function createShipButtons(){
 		var p = document.createElement("p");
 		settings.appendChild(p);
 		settings.appendChild(gen);
+
+		reset = document.createElement("input");
+		reset.setAttribute("type","button");
+		reset.setAttribute("id","bt-zeruj");
+		reset.setAttribute("value","Resetuj");
+		reset.setAttribute("onclick","resetShips()");
+
+		var p = document.createElement("p");
+		settings.appendChild(p);
+		settings.appendChild(reset);
 }
+
+function resetShips(){
+	ships =new Array(4,3,2,1);
+	createArray();
+	for (var i = 1; i <= ships.length; i++) {
+		$("#"+i).val( ships[i-1] );
+	}
+
+	for (var j = 0; j <= 9; j++) {
+		for (var i = 0; i < 9; i++) {
+			var bt = document.getElementById(j+"."+i);
+			bt.disabled = true;
+			bt.setAttribute("style","width: 24px;");
+		}
+	}
+}
+
 
 function sendShip(){
 		//Tu ma byc wysyłanie tablicy sendarr, jak to można wywołać, to jakieś statki są już postawione (na razie przynajmniej jeden)
 		GenOpponentBoard();
+		gen.disabled = true;
+		reset.disabled = true;
 }
 
 function createTable(){
@@ -119,10 +153,10 @@ function setShip(tmp){
 	//clearShadow();
 	shipLength = tmp;
 	if (ships[tmp-1]!=0) {
-		for (var j = 0; j <= 9; j++) {
+		for (var j = 1; j <= 10; j++) {
 			for (var i = 0; i < 9; i++) {
 				if (whitestain[j][i]==1) {
-					var bt = document.getElementById(j+"."+i);
+					var bt = document.getElementById(j-1+"."+i);
 					bt.disabled = false;
 				};
 			}
@@ -133,7 +167,10 @@ function setShip(tmp){
 }
 
 function shipClick(aa){
-
+	var w1 = true;
+	var w2 = true;
+	var w3 = true;
+	var w4 = true;
 	shiptmp = aa;
 	for (var j = 0; j <= 9; j++) {
 		for (var i = 0; i < 9; i++) {
@@ -150,41 +187,73 @@ function shipClick(aa){
 		var tmp  = document.getElementById(aa);
 		tmp.setAttribute("style","background-Color:#556;width: 24px;");
 		if (shipLength>0) {
+			/*
+			for (var i = 0; i <= shipLength; i++) {
+				if (sendarr[y-1+1+i][x]){w1 = false};
+				if (sendarr[y-i][x]){w2 = false};
+				if (sendarr[y][x-1+1+i]){w3 = false};
+				if (sendarr[y][x-i]){w4 = false};
+			};
+*/
 			if (y>shipLength-1) {
-				var bb = y-1+"."+x;
-				var means = document.getElementById(bb);
-				temparr.push(bb);
-				means.disabled = false;
-				means.setAttribute("style","background-Color:#357EC7;width: 24px;");
-				means.setAttribute("onclick","drawShip(this.id)");
+				for (var i = 0; i <= shipLength+1; i++) {
+					if (y>shipLength+1) {
+						if (sendarr[y-i][x]==1){w1 = false};
+					}
+				}
+				if (w1) {			
+					var bb = y-1+"."+x;
+					var means = document.getElementById(bb);
+					temparr.push(bb);
+					means.disabled = false;
+					means.setAttribute("style","background-Color:#357EC7;width: 24px;");
+					means.setAttribute("onclick","drawShip(this.id)");
+				}
+				
 			};
 			if (y<10-shipLength) {
-				var b1 = y-1+2+"."+x; 
-				means = document.getElementById(b1);
-				temparr.push(b1);
-				means.disabled = false;
-				means.setAttribute("style","background-Color:#357EC7;width: 24px;");
-				means.setAttribute("onclick","drawShip(this.id)");
+				for (var i = 0; i <= shipLength+1; i++) {
+					if (sendarr[y-1+1+i][x]==1){w2 = false};
+				}
+				if (w2) {
+					var b1 = y-1+2+"."+x; 
+					means = document.getElementById(b1);
+					temparr.push(b1);
+					means.disabled = false;
+					means.setAttribute("style","background-Color:#357EC7;width: 24px;");
+					means.setAttribute("onclick","drawShip(this.id)");
+				};
+
 			};
 			//console.log(x+"  "+shipLength);
 			if (x>shipLength-1) {
-				var kupa = x-1;
-				var b2 = y+"."+kupa;
-				means = document.getElementById(b2);
-				temparr.push(b2);
-				means.disabled = false;
-				//console.log(aa +"  " + bb +"  "+ b1+"  "+ b2);
-				means.setAttribute("style","background-Color:#357EC7;width: 24px;");
-				means.setAttribute("onclick","drawShip(this.id)");
+				for (var i = 0; i <= shipLength+1; i++) {
+					if (sendarr[y][x-i]==1){w3 = false};
+				}
+				if (w3) {
+					var kupa = x-1;
+					var b2 = y+"."+kupa;
+					means = document.getElementById(b2);
+					temparr.push(b2);
+					means.disabled = false;
+					//console.log(aa +"  " + bb +"  "+ b1+"  "+ b2);
+					means.setAttribute("style","background-Color:#357EC7;width: 24px;");
+					means.setAttribute("onclick","drawShip(this.id)");
+				}
 			};
 			if (x<9-shipLength) {
-				x++;
-				var b3 = y+"."+x;
-				means = document.getElementById(b3);
-				temparr.push(b3);
-				means.disabled = false;
-				means.setAttribute("style","background-Color:#357EC7;width: 24px;");
-				means.setAttribute("onclick","drawShip(this.id)");
+				for (var i = 0; i <= shipLength+1; i++) {
+					if (sendarr[y][x-1+1+i]==1){w4 = false};
+				}
+				if (w4) {
+					x++;
+					var b3 = y+"."+x;
+					means = document.getElementById(b3);
+					temparr.push(b3);
+					means.disabled = false;
+					means.setAttribute("style","background-Color:#357EC7;width: 24px;");
+					means.setAttribute("onclick","drawShip(this.id)");
+				}
 			};
 		}else{
 		ships[shipLength]--;
@@ -193,10 +262,10 @@ function shipClick(aa){
 		whitestain[y][x] = 0;
 		for (var i = 0; i <= 8; i++) {
 			if (i<=shipLength+2){
-				wy = (y-1)+i;
+				wy = (y-1+1)+i;
 				wx = x-1;
 				whitestain[wy][wx] = 0;
-				wy = (y-1)+i;
+				wy = (y-1+1)+i;
 				wx = x;
 				whitestain[wy][wx] = 0;
 				wx = x-1+2;
@@ -235,18 +304,26 @@ gen.disabled = false;
 	};
 
 	sendarr[y][x] = 1;
-
-	for (var i = 0; i <= 8; i++) {
-		if (i<=shipLength+2){
-			wy = (y-1)+i;
-			wx = x-1;
-			whitestain[wy][wx] = 0;
-			wx = x;
-			whitestain[wy][wx] = 0;
-			wx = x-1+2;
-			console.log(wx);
-			whitestain[wy][wx] = 0;
-		}
+	
+	for (var i = 0; i <= shipLength+2; i++) {
+		
+			if (wektorx==0) {
+				if (wektory>0){wy = y-wektory*i+2;}else{wy = y-wektory*i;}
+				wx = x-1;
+				whitestain[wy][wx] = 0;
+				wx = x;
+				whitestain[wy][wx] = 0;
+				wx = x-1+2;
+				whitestain[wy][wx] = 0;
+			}else if (wektory==0) {
+				if (wektorx>0){wx = x-wektorx*i+1;}else{wx = x-wektorx*i-1;}
+				wy = y;
+				whitestain[wy][wx] = 0;
+				wy = y-1+2;
+				whitestain[wy][wx] = 0;
+				wy = y-1+3;
+				whitestain[wy][wx] = 0;
+			}
 	};
 
 	for (var i = 1; i <= shipLength; i++) {
@@ -266,8 +343,6 @@ gen.disabled = false;
 			sendarr[y][x] = 1;
 		};
 	};
-
-
 	//console.log(shipLength);
 	ships[shipLength]--;
 	spadaj = shipLength+1;
